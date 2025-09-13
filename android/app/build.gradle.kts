e93e6faf8ac7a71576eb1a3ffff7b3b2e528f0c1
Pukpixed <rmenavijitnavee@gmail.com>
2025-09-13 14:25:58 +0700

    Initial commit: Flutter project setup

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.learn_flutter_67_12"
+    compileSdk = flutter.compileSdkVersion
+    // ✅ ใช้ NDK เวอร์ชันที่ plugin ต้องการ
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.learn_flutter_67_12"
+        // ✅ ต้องใช้ minSdk = 23 สำหรับ cloud_firestore
+        minSdk = 23
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
